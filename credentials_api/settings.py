290845df0f9addd9ef57c737997fddad995094f3
peter isaiah <49757859+isaiahpeter@users.noreply.github.com>
2026-01-31 00:34:06 +0100

    Update STATIC_URL and add STATIC_ROOT configuration

--- credentials_api/settings.py
+++ credentials_api/settings.py
@@ -123,7 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 REST_FRAMEWORK = {
         "DEFAULT_PERMISSION_CLASSES":[],
